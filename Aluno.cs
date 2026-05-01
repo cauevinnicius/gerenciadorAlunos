7eee7b60aa8872b7358492292bd68a16072fee9c
cauevinnicius <135167189+cauevinnicius@users.noreply.github.com>
2026-05-01 11:33:16 -0300

    Delete Aluno.cs

--- Aluno.cs
+++
@@ -1,9 +0,0 @@
-// meu molde de aluno
-public class Aluno
-{
-    public int Id { get; set; }
-    public string Nome { get; set; }
-    public string Cpf { get; set; }
-    public string Email { get; set; }
-    public string Celular { get; set; }
-}
