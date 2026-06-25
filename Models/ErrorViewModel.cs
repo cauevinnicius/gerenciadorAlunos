b1ffafcb38fd1e1e97596dbbf38e65f23b6b4921
cauevinnicius <135167189+cauevinnicius@users.noreply.github.com>
2026-06-24 23:12:08 -0300

    Delete Models/ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace GerenciadorAlunosV2.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
