6340558bc3c97aa731f750ba25ddb7278e99a221
cauevinnicius <135167189+cauevinnicius@users.noreply.github.com>
2026-06-24 23:11:51 -0300

    Delete ViewModels directory

--- ViewModels/AlunoListaViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace GerenciadorAlunosV2.ViewModels;
-public class AlunoListaViewModel
-{   public int IdAluno { get; set; }
-    public string NomeAluno { get; set; }
-    public string CpfAluno { get; set; }  
-    public string EmailAluno { get; set; }
-    public string CelularAluno { get; set; }
-}
